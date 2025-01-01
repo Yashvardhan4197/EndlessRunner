d1ce008f56fb4cef57cf066dc33ae5d08f161aac
Yashvardhan4197 <20BCS4197@cuchd.in>
2025-01-01 17:39:08 +0530

    Added generic object pool utility(not configured yet to existing pools)

---
+++ Assets/Scripts/Utilities/GenericPool.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+public class GenericPool<T> where T : class
+{
+    private List<PooledItem<T>> pooledItems=new List<PooledItem<T>>();
+
+
+
+    public T GetItem()
+    {
+        PooledItem<T> item = pooledItems.Find(item=>!item.isUsed);
+        if(item!=null)
+        {
+            item.isUsed = true;
+            return item.controller;
+        }
+        return CreatePooledItem();
+    }
+
+    public void ReturnToPool(T returnItem)
+    {
+        PooledItem<T> item= pooledItems.Find(item=>item.controller== returnItem);
+        if(item!=null)
+        {
+            item.isUsed= false;
+        }
+    }
+
+
+    private T CreatePooledItem()
+    {
+        PooledItem<T> newItem= new PooledItem<T>();
+        newItem.isUsed = true;
+        newItem.controller = CreateItem();
+        pooledItems.Add(newItem);
+        return newItem.controller;
+    }
+
+    public virtual T CreateItem()
+    {
+        throw new NotImplementedException("child does not contain implementation");
+    }
+
+    public class PooledItem<T>
+    {
+        public T controller;
+        public bool isUsed;
+    }
+}
+
